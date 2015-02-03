87ae3bc2cc7be860741601bb8f274adbf39a588f
Lukasz Pacyk <lpacyk31@gmail.com>
2015-02-02 17:08:14 -0800

    Changed all references of rythmic into Rhythmic
    Separated fragments into their own xml and java classes
    
    Home Page
    Music Page
    Statistics Page
    Settings Page
    
    Implemented the instantiation of each fragment into each tab.

---
+++ app/src/main/java/com/rhythmictracks/rhythmictracks/SettingsPageFragment.java
@@ -0,0 +1,37 @@
+package com.rhythmictracks.rhythmictracks;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsPageFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static SettingsPageFragment newInstance(int sectionNumber) {
+        SettingsPageFragment fragment = new SettingsPageFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public SettingsPageFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_settings_page, container, false);
+        return rootView;
+    }
+}
